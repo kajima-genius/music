6a6e12787a57c9d7ad975f291f62965343f4094a
Alex <osikov.alex2001@gmail.com>
2021-08-01 13:19:29 +0300

    creating a mapper with mapstruct

--- src/main/java/com/example/music/backend/user/service/UserServiceImpl.java
+++ src/main/java/com/example/music/backend/user/service/UserServiceImpl.java
@@ -1,21 +1,19 @@
 package com.example.music.backend.user.service;
 
-import com.example.music.backend.user.converter.UserDtoConverter;
+import com.example.music.backend.user.converter.UserDtoMapper;
 import com.example.music.backend.user.domain.User;
 import com.example.music.backend.user.dto.UserDto;
 import com.example.music.backend.user.exception.UserAlreadyExistException;
 import com.example.music.backend.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     private boolean emailExist(String email) {
         return userRepository.findByEmail(email).isPresent();
@@ -33,7 +31,7 @@ public class UserServiceImpl implements UserService {
                     + dto.getEmail());
         }
 
-        User newUser = UserDtoConverter.toEntity(dto);
+        User newUser = UserDtoMapper.INSTANCE.toEntity(dto);
         return userRepository.save(newUser);
     }
 
