2aee54aeb2cc8b63d36f438083cf49188ead2557
Alex <osikov.alex2001@gmail.com>
2021-08-09 11:30:48 +0300

    changed UserController: 1) Changed return 2) Error handling is now in the service

--- src/main/java/com/example/music/backend/user/controller/UserController.java
+++ src/main/java/com/example/music/backend/user/controller/UserController.java
@@ -3,60 +3,39 @@ package com.example.music.backend.user.controller;
 import com.example.music.backend.verification.OnRegistrationCompleteEvent;
 import com.example.music.backend.user.domain.User;
 import com.example.music.backend.user.dto.UserDto;
-import com.example.music.backend.user.exception.UserAlreadyExistException;
 import com.example.music.backend.user.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.net.URI;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private ApplicationEventPublisher eventPublisher;
     private final UserService service;
     private final PasswordEncoder encoder;
 
-    @PostMapping(consumes = APPLICATION_JSON_VALUE, path = "/user/registration")
-    public ResponseEntity<HttpStatus> create(@RequestBody UserDto userDto,
-                                             HttpServletRequest request) {
-        HttpStatus status;
-        try {
-            String encodedPassword = encoder.encode(userDto.getPassword());
-            userDto.setPassword(encodedPassword);
-            User saved = service.create(userDto);
-            String appUrl = request.getContextPath();
-            eventPublisher.publishEvent(new OnRegistrationCompleteEvent(saved, request.getLocale(), appUrl));
-            status = HttpStatus.OK;
-        } catch (UserAlreadyExistException uaeEx) {
-            status = HttpStatus.BAD_REQUEST;
-        } catch (RuntimeException ex) {
-            status = HttpStatus.BAD_REQUEST;
-        }
-        return ResponseEntity.status(status).build();
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, value = "/registration")
+    public ResponseEntity<URI> create(@RequestBody UserDto userDto,
+                                      HttpServletRequest request) {
+        String encodedPassword = encoder.encode(userDto.getPassword());
+        userDto.setPassword(encodedPassword);
+        User saved = service.create(userDto);
+        String appUrl = request.getContextPath();
+        eventPublisher.publishEvent(new OnRegistrationCompleteEvent(saved, request.getLocale(), appUrl));
+        return ResponseEntity.created(URI.create("/users" + saved.getId())).build();
     }
-
-//    @GetMapping(path = "/user/registration")
-//    public String getPage(Model model) {
-//        model.addAttribute("UserDto", new UserDto());
-//        return "registration";
-//    }
-//
-//    @GetMapping(path = "/login")
-//    public String getLoginPage() {
-//        return "login";
-//    }
-//
-//    @GetMapping(path = "/home")
-//    public String getHomePage() {
-//        return "home";
-//    }
 }
+
