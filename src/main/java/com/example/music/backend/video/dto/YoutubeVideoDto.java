2603305555cbbd66b7046eaa6b0990a23e2b4e0d
Alex <osikov.alex2001@gmail.com>
2021-08-30 11:09:05 +0300

    1) Api connection 2) Created its own logic for the video

---
+++ src/main/java/com/example/music/backend/video/dto/YoutubeVideoDto.java
@@ -0,0 +1,13 @@
+package com.example.music.backend.video.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class YoutubeVideoDto {
+
+    private String youtubeId;
+}
---
+++ src/main/java/com/example/music/backend/video/service/YoutubeVideoService.java
@@ -0,0 +1,14 @@
+package com.example.music.backend.video.service;
+
+import com.example.music.backend.video.response.YoutubeVideoResponse;
+
+import java.util.List;
+
+public interface YoutubeVideoService {
+
+    List<YoutubeVideoResponse> searchYoutubeVideo(String queryTerm, Long maxResults);
+
+    List<YoutubeVideoResponse> getYoutubeVideoTrends(Long maxResults);
+
+    YoutubeVideoResponse getYoutubeVideo(String youtubeId);
+}
