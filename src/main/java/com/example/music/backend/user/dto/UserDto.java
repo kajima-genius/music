f15114dd0b5df74fb4de66de8210846dccc37f5f
Alex <osikov.alex2001@gmail.com>
2021-08-30 10:54:08 +0300

    1) Added logic with jwt 2) Changed WebSecurityConfig settings with new jwt logic 3) A new error has been added to the user 4) Added response class for user

--- src/main/java/com/example/music/backend/user/dto/UserDto.java
+++ src/main/java/com/example/music/backend/user/dto/UserDto.java
@@ -2,85 +2,21 @@ package com.example.music.backend.user.dto;
 
 import com.example.music.backend.user.domain.Gender;
 import com.example.music.backend.user.domain.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.Date;
-
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserDto {
 
-    private String userName;
+    private String username;
     private Gender gender;
     private Long id;
     private String email;
     private String password;
-    private Role role;
-    private String dateOfBirch;
-
-    public UserDto() {
-    }
-
-    public UserDto(String userName, Gender gender, String email, String password, String dateOfBirch) {
-        this.userName = userName;
-        this.gender = gender;
-        this.email = email;
-        this.password = password;
-        this.dateOfBirch = dateOfBirch;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDateOfBirch() {
-        return dateOfBirch;
-    }
-
-    public void setDateOfBirch(String dateOfBirch) {
-        this.dateOfBirch = dateOfBirch;
-    }
-
-
+    private Role role = Role.USER;
+    private String dateOfBirth;
 }
