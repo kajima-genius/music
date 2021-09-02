f5b7aaa6f0abcfc3fa8cc9897f65bf3d4da3e348
Alex <osikov.alex2001@gmail.com>
2021-09-02 11:55:00 +0300

    some commit

--- src/main/java/com/example/music/MusicApplication.java
+++ src/main/java/com/example/music/MusicApplication.java
@@ -2,14 +2,9 @@ package com.example.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class MusicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicApplication.class, args);
-	}
-
+	public static void main(String[] args) {SpringApplication.run(MusicApplication.class, args);}
 }
